4f0816120038d8815da6fedc459b8dbdb4b0293c
samibhatti <abdulsami.bhatti@gmail.com>
2016-12-28 09:43:27 +0100

    Add files via upload

---
+++ hello/hello/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your name ");
+
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Hello," + name);
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            if (hoursOfSleep > 8)
+
+            {
+                Console.WriteLine("You are well rested");
+
+            }
+
+            else
+
+            {
+                Console.WriteLine("you need more sleep");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
